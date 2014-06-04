9b4002d95a1fcb2b1bf53c87ab420ecf54571ae7
Joachim Hill-Grannec <joachim@suvault.com>
2014-06-04 16:11:41 -0700

    first commit

---
+++ SampleSwift/NewViewController.swift
@@ -0,0 +1,23 @@
+//
+//  NewViewController.swift
+//  SampleSwift
+//
+//  Created by Joachim Hill-Grannec on 6/2/14.
+//  Copyright (c) 2014 liffft. All rights reserved.
+//
+
+import UIKit
+import Foundation
+import SceneKit
+
+class NewViewController: UIViewController {
+
+    
+    override func awakeFromNib() {
+        
+
+        
+        
+    }
+    
+}
---
+++ SampleSwift/ViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ViewController.swift
+//  SampleSwift
+//
+//  Created by Joachim Hill-Grannec on 6/2/14.
+//  Copyright (c) 2014 liffft. All rights reserved.
+//
+
+import UIKit
+
+class 💩: UIViewController {
+
+    
+    @IBOutlet var countLabel : UILabel
+    @IBAction func incrementButton(sender : UIButton) {
+        self.count++
+    }
+    var count: Int = 0 {
+        didSet {
+            self.countLabel.text = String(self.count)
+        }
+    }
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
